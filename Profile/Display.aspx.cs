d615511b9022e6016d81b91e4523763ad9daeb70
gayathrisanthanakrishnan <33493539+gayathrisanthanakrishnan@users.noreply.github.com>
2019-05-29 15:32:22 -0400

    Add files via upload

---
+++ Profile/Display.aspx.cs
@@ -0,0 +1,103 @@
+﻿/*  
+ 
+    Copyright (c) 2008-2012 by the President and Fellows of Harvard College. All rights reserved.  
+    Profiles Research Networking Software was developed under the supervision of Griffin M Weber, MD, PhD.,
+    and Harvard Catalyst: The Harvard Clinical and Translational Science Center, with support from the 
+    National Center for Research Resources and Harvard University.
+
+
+    Code licensed under a BSD License. 
+    For details, see: LICENSE.txt 
+  
+*/
+
+using System;
+using System.Web.UI;
+using System.Xml;
+using System.Web.UI.HtmlControls;
+
+using Profiles.Profile.Utilities;
+using Profiles.Framework.Utilities;
+
+namespace Profiles.Profile
+{
+    public partial class Display : ProfileData
+    {
+        private Profiles.Framework.Template masterpage;
+
+        public void Page_Load(object sender, EventArgs e)
+        {
+            UserHistory uh = new UserHistory();
+
+            masterpage = (Framework.Template)base.Master;
+            this.Master = masterpage;
+
+            this.LoadAssets();
+
+            this.LoadPresentationXML();
+
+            XmlNode x = this.PresentationXML.SelectSingleNode("Presentation[1]/ExpandRDFList[1]");
+
+            if (x != null)
+                base.RDFTriple.ExpandRDFList = x.InnerXml;
+
+            if (base.RDFTriple.Subject != 0 && base.RDFTriple.Predicate != 0 && base.RDFTriple.Object == 0)
+                base.RDFTriple.Limit = "1";
+
+            base.LoadRDFData();
+
+            Framework.Utilities.DebugLogging.Log("Page_Load Profile 1: " + DateTime.Now.ToLongTimeString());
+
+            masterpage.Tab = base.Tab;
+            masterpage.RDFData = base.RDFData;
+            masterpage.RDFNamespaces = base.RDFNamespaces;
+            masterpage.PresentationXML = this.PresentationXML;
+            Framework.Utilities.DebugLogging.Log("Page_Load Profile 2: " + DateTime.Now.ToLongTimeString());
+
+            // UCSF added schema.org info
+            // Only do this for a person only version of this page !
+            XmlNode presentationClass = PresentationXML.SelectSingleNode("//Presentation/@PresentationClass", base.RDFNamespaces);
+            if (presentationClass != null && "profile".Equals(presentationClass.InnerText.ToLower()) &&
+                this.RDFData.SelectSingleNode("rdf:RDF[1]/rdf:Description[1]/foaf:firstName", base.RDFNamespaces) != null &&
+                this.RDFData.SelectSingleNode("rdf:RDF[1]/rdf:Description[1]/foaf:lastName", base.RDFNamespaces) != null)
+            {
+                ((HtmlGenericControl)masterpage.FindControl("divProfilesContentMain")).Attributes.Add("itemscope", "itemscope");
+                ((HtmlGenericControl)masterpage.FindControl("divProfilesContentMain")).Attributes.Add("itemtype", "http://schema.org/Person");
+
+                HtmlMeta Description = new HtmlMeta();
+                Description.Name = "Description";
+                string name = this.RDFData.SelectSingleNode("rdf:RDF[1]/rdf:Description[1]/foaf:firstName", base.RDFNamespaces).InnerText + " " +
+                     this.RDFData.SelectSingleNode("rdf:RDF[1]/rdf:Description[1]/foaf:lastName", base.RDFNamespaces).InnerText;
+                Description.Content = name + "'s profile, publications, research topics, and co-authors";
+                Page.Header.Controls.Add(Description);
+
+                HtmlLink Canonical = new HtmlLink();
+                Canonical.Href = Request.Url.AbsoluteUri.IndexOf('?') == -1 ? Request.Url.AbsoluteUri.ToLower() : Request.Url.AbsoluteUri.ToLower().Substring(0, Request.Url.AbsoluteUri.IndexOf('?'));
+                Canonical.Attributes["rel"] = "canonical";
+                Page.Header.Controls.Add(Canonical);
+            }
+        }
+
+
+        private void LoadAssets()
+        {
+
+            HtmlLink Displaycss = new HtmlLink();
+            Displaycss.Href = Root.Domain + "/Profile/CSS/display.css";
+            Displaycss.Attributes["rel"] = "stylesheet";
+            Displaycss.Attributes["type"] = "text/css";
+            Displaycss.Attributes["media"] = "all";
+            Page.Header.Controls.Add(Displaycss);
+        }
+
+        public void LoadPresentationXML()
+        {
+            Profiles.Profile.Utilities.DataIO data = new Profiles.Profile.Utilities.DataIO();
+            this.PresentationXML = data.GetPresentationData(this.RDFTriple);
+        }
+
+        public XmlDocument PresentationXML { get; set; }
+        public Profiles.Framework.Template Master { get; set; }
+
+    }
+}
---
+++ Profile/Profile.aspx.cs
@@ -0,0 +1,74 @@
+﻿/*  
+ 
+    Copyright (c) 2008-2012 by the President and Fellows of Harvard College. All rights reserved.  
+    Profiles Research Networking Software was developed under the supervision of Griffin M Weber, MD, PhD.,
+    and Harvard Catalyst: The Harvard Clinical and Translational Science Center, with support from the 
+    National Center for Research Resources and Harvard University.
+
+
+    Code licensed under a BSD License. 
+    For details, see: LICENSE.txt 
+  
+*/
+using System;
+using System.Web;
+
+
+using Profiles.Profile.Utilities;
+
+namespace Profiles.Profile
+{
+    public partial class Profile : ProfileData
+    {
+        static private Random random = new Random();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            Framework.Utilities.RDFTriple request = base.RDFTriple;
+
+                       //The system default is True and True for showdetails and expand, but if its an external page call to this page, 
+            //then its set to false for expand.           
+            if (HttpContext.Current.Request.Headers["Expand"] != null)
+                this.RDFTriple.Expand = Convert.ToBoolean(HttpContext.Current.Request.Headers["Expand"].ToString());
+            else
+                this.RDFTriple.Expand = true;
+
+            if (HttpContext.Current.Request.Headers["ShowDetails"] != null)
+                this.RDFTriple.ShowDetails = Convert.ToBoolean(HttpContext.Current.Request.Headers["ShowDetails"].ToString());
+            else
+                this.RDFTriple.ShowDetails = true;
+
+            if (HttpContext.Current.Request.Headers["Offset"] != null)
+                this.RDFTriple.Offset = HttpContext.Current.Request.Headers["Offset"].ToString();
+
+            if (HttpContext.Current.Request.Headers["Limit"] != null)
+                this.RDFTriple.Limit = HttpContext.Current.Request.Headers["Limit"].ToString();
+
+            if (HttpContext.Current.Request.Headers["ExpandRDFListXML"] != null)
+                this.RDFTriple.ExpandRDFList = HttpContext.Current.Request.Headers["ExpandRDFListXML"].ToString();
+
+            if (HttpContext.Current.Request.Headers["SessionID"] != null)
+                this.RDFTriple.Session.SessionID = HttpContext.Current.Request.Headers["SessionID"].ToString();
+
+
+            if (HttpContext.Current.Request.Headers["UserAgent"] != null)
+                this.RDFTriple.Session.UserAgent = HttpContext.Current.Request.Headers["UserAgent"].ToString();
+
+
+            base.LoadRDFData();
+
+
+            Response.Clear();
+            Response.ContentType = "application/rdf+xml";
+            Response.Charset = "charset=UTF-8";
+            Response.StatusCode = Convert.ToInt16("200");
+
+            // Tell the bots that this is slow moving data, add an exires at some random date up to 30 days out
+            DateTime expires = DateTime.Now.Add(TimeSpan.FromDays(random.NextDouble() * 29 + 1));
+            Response.AddHeader("Expires", expires.ToUniversalTime().ToString("r"));
+
+            Response.Write(base.RDFData.InnerXml);
+        }
+    }
+}
